4492e6a370696ff7f471a8778ec3fe91810d13e3
Do Hoang Giang <gd160299@gmail.com>
2024-10-03 14:36:38 +0700

    add config reloadable for config external

--- src/main/java/org/pj/config/db/DbConfig.java
+++ src/main/java/org/pj/config/db/DbConfig.java
@@ -2,31 +2,48 @@ package org.pj.config.db;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.aeonbits.owner.ConfigFactory;
 import org.pj.config.AppConfig;
+import org.pj.config.ConfigWatcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DbConfig {
-    private static final HikariDataSource dataSource;
+    private static HikariDataSource dataSource;
+    private static final AppConfig config;
+    private static final Logger logger = LoggerFactory.getLogger(DbConfig.class);
 
     static {
-        AppConfig config = ConfigFactory.create(AppConfig.class);
+        ConfigWatcher configWatcher = new ConfigWatcher();
+        config = configWatcher.getAppConfig();
+        initializePool();
+    }
+
+    private static void initializePool() {
         HikariConfig hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl(config.dbUrl());
         hikariConfig.setUsername(config.dbUsername());
         hikariConfig.setPassword(config.dbPassword());
         hikariConfig.setDriverClassName(config.dbDriverClassName());
-        hikariConfig.setMaximumPoolSize(config.dbMaxPoolSize()); // So luong ket noi toi da
-        hikariConfig.setMinimumIdle(config.dbMinIdle()); // So luong ket noi toi thieu
-        hikariConfig.setIdleTimeout(config.dbIdleTimeout()); // Thoi gian mot ket noi duoc giu lai khi khong su dung
-        hikariConfig.setMaxLifetime(config.dbMaxLifetime()); // Thoi gian toi da mot ket noi duoc su dung
-        hikariConfig.setConnectionTimeout(config.dbConnectionTimeout()); // Thoi gian toi da de lay duoc ket noi tu pool
+        hikariConfig.setMaximumPoolSize(config.dbMaxPoolSize());
+        hikariConfig.setMinimumIdle(config.dbMinIdle());
+        hikariConfig.setIdleTimeout(config.dbIdleTimeout());
+        hikariConfig.setMaxLifetime(config.dbMaxLifetime());
+        hikariConfig.setConnectionTimeout(config.dbConnectionTimeout());
+        if (dataSource != null) {
+            dataSource.close();
+        }
         dataSource = new HikariDataSource(hikariConfig);
+        logger.info("Db initialized with updated config");
     }
 
     public static Connection getConnection() throws SQLException {
         return dataSource.getConnection();
+    }
+
+    public static void reloadConfig() {
+        initializePool();
     }
 }
